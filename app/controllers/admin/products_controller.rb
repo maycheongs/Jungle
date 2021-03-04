55c41065bcea04c81c65ee92b8b93b6bb1a43fc6
May C <maycheongs@gmail.com>
2021-03-04 00:42:37 +0000

    edit admin auth to use env

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
-
-  http_basic_authenticate_with name: 'Jungle', password: 'book'
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
+                               password: ENV['HTTP_BASIC_PASSWORD'],
+                               if: -> { ENV['HTTP_BASIC_PASSWORD'].present? }
 
   def index
     @products = Product.order(id: :desc).all
@@ -14,7 +15,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to %i[admin products], notice: 'Product created!'
     else
       render :new
     end
@@ -23,20 +24,14 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product = Product.find params[:id]
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to %i[admin products], notice: 'Product deleted!'
   end
 
   private
 
   def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
+    params
+      .require(:product)
+      .permit(:name, :description, :category_id, :quantity, :image, :price)
   end
-
 end
